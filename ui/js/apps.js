4cf3f16b7b57121d3041ca3ec92977cbbe059711
GeoffH02 <geoffrey02@orange.fr>
2022-03-01 16:06:21 +0100

    implémentations de la page apps et de ces sous catégories

--- ui/js/apps.js
+++ ui/js/apps.js
@@ -3,11 +3,13 @@ $(".footerElem").click(function () {
 
     if (data === "store") {
         $("#mainPane .header .stats").hide()
+        $("#mainPane .header ").hide()
         $("#mainPane .header").css({"height" : "auto","margin-bottom":"0px"})
     }
 
     if (data !== "store" && $("#mainPane .header .stats").is(":hidden")) {
         $("#mainPane .header .stats").show()
+        $("#mainPane .header ").show()
         $("#mainPane .header").css({"height" : "110px","margin-bottom":"3.5em"})
 
 
