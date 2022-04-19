7bcd8b1a32aacf8ef915485dc4d76b2835addbf0
GeoffH02 <geoffrey02@orange.fr>
2022-04-19 16:36:15 +0200

    new versionning

---
+++ ui/js/addAssets.js
@@ -0,0 +1,22 @@
+$("#assetsMenu").click(function () {
+/*    $(".header").hide()
+    $("#body .bodyElem.resume").hide()
+    $("#assetList").hide()*/
+    $(".addAssetMenu").show()
+
+})
+
+/*
+$(".searchIcon").click(function () {
+    $("#assetList").show()
+    $(".inputSearch").addClass("inputOpen")
+
+    $(".searchAssets").hide()
+})
+
+$("#closeSearchAssets").click(function () {
+    $("#assetList").hide()
+    $(".inputSearch").removeClass("inputOpen")
+
+    $(".searchAssets").show()
+})*/
