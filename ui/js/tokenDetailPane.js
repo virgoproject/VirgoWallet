c7f50c91bc84443ffe665711e3383c109c328126
praxls <lalin.hugo@gmail.com>
2022-08-10 12:17:43 +0200

    wip

--- ui/js/tokenDetailPane.js
+++ ui/js/tokenDetailPane.js
@@ -2,29 +2,38 @@ class TokenDetailPane {
 
     static self = $("#tokenDetailPane")
     static back = $("#tokenDetailPane .back")
-    static name = $("#tokenDetailPane .name")
-    static menu = {
-        news: $("#tokenNewsBtn"),
-        infos: $("#tokenInfosBtn")
+    static loading = $("#tokenDetailsPaneLoading")
+    static simplePane = {
+        self: $("#tokenDetailsSimple")
     }
-    static news = $("#tokenNews")
-    static infos = $("#tokenInfos")
-    static chart = $("#tokenDetailPane .chart")
-    static chartLoading = $("#tokenDetailPane .chartLoading")
-    static chartInfos = {
-        name: $("#tokenDetailPane .infos .name"),
-        logo: $("#tokenDetailPane .infos .logo"),
-        price: $("#tokenDetailPane .infos .price"),
-        change: $("#tokenDetailPane .infos .change"),
-        changeVal: $("#tokenDetailPane .infos .change val")
+    static detailedPane = {
+        self: $("#tokenDetailsDetailed"),
+        name: $("#tokenDetailPane .name"),
+        menu: {
+            news: $("#tokenNewsBtn"),
+            infos: $("#tokenInfosBtn")
+        },
+        news: $("#tokenNews"),
+         infos: $("#tokenInfos"),
+         chart: $("#tokenDetailPane .chart"),
+         chartLoading: $("#tokenDetailPane .chartLoading"),
+         chartInfos: {
+            name: $("#tokenDetailPane .infos .name"),
+            logo: $("#tokenDetailPane .infos .logo"),
+            price: $("#tokenDetailPane .infos .price"),
+            change: $("#tokenDetailPane .infos .change"),
+            changeVal: $("#tokenDetailPane .infos .change val")
+        },
+        periodBtns: $("#tokenDetailPane .periodBtns .periodBtn"),
+        newsContainer: $("#tokenNews .newsContainer"),
+        newsLoading: $("#tokenNewsLoading"),
+        sampleNews: $("#sampleTokenNews"),
+        infosAdditional: $("#tokenInfos .additional"),
+        infosAdditionalBtn: $("#tokenInfos .additionalBtn"),
+        descReadMore: $("#tokenInfos .readmore"),
+        infosLoading: $("#tokenInfosLoading"),
+        infosWrapper: $("#tokenInfosWrapper")
     }
-    static periodBtns = $("#tokenDetailPane .periodBtns .periodBtn")
-    static newsContainer = $("#tokenNews .newsContainer")
-    static newsLoading = $("#tokenNewsLoading")
-    static sampleNews = $("#sampleTokenNews")
-    static infosAdditional = $("#tokenInfos .additional")
-    static infosAdditionalBtn = $("#tokenInfos .additionalBtn")
-
 
     constructor() {
         const _this = this
@@ -33,76 +42,83 @@ class TokenDetailPane {
             TokenDetailPane.self.hide()
         })
 
-        TokenDetailPane.menu.news.click(function(){
-            TokenDetailPane.menu.news.addClass("selected")
-            TokenDetailPane.menu.infos.removeClass("selected")
-            TokenDetailPane.news.show()
-            TokenDetailPane.infos.hide()
+        TokenDetailPane.detailedPane.menu.news.click(function(){
+            TokenDetailPane.detailedPane.menu.news.addClass("selected")
+            TokenDetailPane.detailedPane.menu.infos.removeClass("selected")
+            TokenDetailPane.detailedPane.news.show()
+            TokenDetailPane.detailedPane.infos.hide()
         })
 
-        TokenDetailPane.menu.infos.click(function(){
-            TokenDetailPane.menu.infos.addClass("selected")
-            TokenDetailPane.menu.news.removeClass("selected")
-            TokenDetailPane.infos.show()
-            TokenDetailPane.news.hide()
+        TokenDetailPane.detailedPane.menu.infos.click(function(){
+            TokenDetailPane.detailedPane.menu.infos.addClass("selected")
+            TokenDetailPane.detailedPane.menu.news.removeClass("selected")
+            TokenDetailPane.detailedPane.infos.show()
+            TokenDetailPane.detailedPane.news.hide()
         })
 
-        TokenDetailPane.periodBtns.click(function(){
+        TokenDetailPane.detailedPane.periodBtns.click(function(){
             if($(this).hasClass("active")) return
 
-            TokenDetailPane.periodBtns.removeClass("active")
+            TokenDetailPane.detailedPane.periodBtns.removeClass("active")
             $(this).addClass("active")
             _this.initChart($(this).attr("data-period"))
         })
 
-        TokenDetailPane.chart.on("mouseout", function(e){
+        TokenDetailPane.detailedPane.chart.on("mouseout", function(e){
             setTimeout(function(){
-                TokenDetailPane.chartInfos.price.html("$" + Utils.beautifyAmount(_this.data.price))
-                TokenDetailPane.chartInfos.changeVal.html(Math.abs(_this.data.change).toFixed(2))
+                TokenDetailPane.detailedPane.chartInfos.price.html("$" + Utils.beautifyAmount(_this.data.price))
+                TokenDetailPane.detailedPane.chartInfos.changeVal.html(Math.abs(_this.data.change).toFixed(2))
                 if(_this.data.change >= 0)
-                    TokenDetailPane.chartInfos.change.removeClass("negative")
+                    TokenDetailPane.detailedPane.chartInfos.change.removeClass("negative")
                 else
-                    TokenDetailPane.chartInfos.change.addClass("negative")
+                    TokenDetailPane.detailedPane.chartInfos.change.addClass("negative")
             }, 10)
         })
 
-        TokenDetailPane.infosAdditionalBtn.click(function(){
-            if(TokenDetailPane.infosAdditionalBtn.hasClass("active")){
-                TokenDetailPane.infosAdditionalBtn.removeClass("active")
-                TokenDetailPane.infosAdditional.css("display", "none")
+        TokenDetailPane.detailedPane.infosAdditionalBtn.click(function(){
+            if(TokenDetailPane.detailedPane.infosAdditionalBtn.hasClass("active")){
+                TokenDetailPane.detailedPane.infosAdditionalBtn.removeClass("active")
+                TokenDetailPane.detailedPane.infosAdditional.css("display", "none")
             }else{
-                TokenDetailPane.infosAdditionalBtn.addClass("active")
-                TokenDetailPane.infosAdditional.css("display", "contents")
+                TokenDetailPane.detailedPane.infosAdditionalBtn.addClass("active")
+                TokenDetailPane.detailedPane.infosAdditional.css("display", "contents")
             }
         })
+
+        TokenDetailPane.detailedPane.descReadMore.click(function(){
+            window.open("https://www.coingecko.com/en/coins/"+_this.tokenInfos.CG_ID, "_blank")
+        })
+
 
     }
 
     displayToken(data){
-        TokenDetailPane.chartInfos.name.html(data.name)
-        TokenDetailPane.chartInfos.price.html("$" + Utils.beautifyAmount(data.price))
+        TokenDetailPane.detailedPane.menu.news.click()
+        TokenDetailPane.detailedPane.chartInfos.name.html(data.name)
+        TokenDetailPane.detailedPane.chartInfos.price.html("$" + Utils.beautifyAmount(data.price))
 
         if(MAIN_ASSET.contract == data.contract)
-            TokenDetailPane.chartInfos.logo.css("background-image", "url('https://raw.githubusercontent.com/virgoproject/tokens/main/" + MAIN_ASSET.ticker + "/" + data.ticker + "/logo.png')");
+            TokenDetailPane.detailedPane.chartInfos.logo.css("background-image", "url('https://raw.githubusercontent.com/virgoproject/tokens/main/" + MAIN_ASSET.ticker + "/" + data.ticker + "/logo.png')");
         else
-            TokenDetailPane.chartInfos.logo.css("background-image", "url('https://raw.githubusercontent.com/virgoproject/tokens/main/" + MAIN_ASSET.ticker + "/" + data.contract + "/logo.png')");
+            TokenDetailPane.detailedPane.chartInfos.logo.css("background-image", "url('https://raw.githubusercontent.com/virgoproject/tokens/main/" + MAIN_ASSET.ticker + "/" + data.contract + "/logo.png')");
 
-        TokenDetailPane.chartInfos.changeVal.html(Math.abs(data.change).toFixed(2))
+        TokenDetailPane.detailedPane.chartInfos.changeVal.html(Math.abs(data.change).toFixed(2))
         if(data.change >= 0)
-            TokenDetailPane.chartInfos.change.removeClass("negative")
+            TokenDetailPane.detailedPane.chartInfos.change.removeClass("negative")
         else
-            TokenDetailPane.chartInfos.change.addClass("negative")
+            TokenDetailPane.detailedPane.chartInfos.change.addClass("negative")
 
         this.data = data
-        this.initChart(1);
-        this.initNews()
-
+        TokenDetailPane.loading.show()
+        TokenDetailPane.simplePane.self.hide()
+        TokenDetailPane.detailedPane.self.hide()
         TokenDetailPane.self.show()
+        this.initChart(1)
     }
 
     initChart(period){
-        TokenDetailPane.chart.html("")
-        TokenDetailPane.chartLoading.show()
+        TokenDetailPane.detailedPane.chart.html("")
+        TokenDetailPane.detailedPane.chartLoading.show()
 
         let contractAddr = this.data.contract
         if(MAIN_ASSET.contract == this.data.contract)
@@ -113,15 +129,26 @@ class TokenDetailPane {
         if(this.tokenInfos === undefined || this.tokenInfos.contract != this.data.contract)
             fetch("https://raw.githubusercontent.com/virgoproject/tokens/main/"+MAIN_ASSET.ticker+"/"+contractAddr+"/infos.json")
                 .then(function(resp){
+                    if(!resp.ok){
+                        TokenDetailPane.simplePane.self.show()
+                        TokenDetailPane.loading.hide()
+                        return
+                    }
                     resp.json().then(function(tokenInfos){
                         _this.tokenInfos = tokenInfos
                         _this.fetchAndDisplayChart(_this.data, tokenInfos, period)
                         _this.initInfos()
+                        _this.initNews()
+                        TokenDetailPane.detailedPane.self.show()
+                        TokenDetailPane.loading.hide()
                     })
                 })
         else {
             this.fetchAndDisplayChart(this.data, this.tokenInfos, period)
             _this.initInfos()
+            _this.initNews()
+            TokenDetailPane.detailedPane.self.show()
+            TokenDetailPane.loading.hide()
         }
     }
 
@@ -134,9 +161,9 @@ class TokenDetailPane {
                 const entries = new Map(chartData)
                 const obj = Object.fromEntries(entries)
 
-                TokenDetailPane.chart.append("<canvas></canvas>")
+                TokenDetailPane.detailedPane.chart.append("<canvas></canvas>")
 
-                const ctxs = TokenDetailPane.chart.find("canvas").get(0).getContext('2d')
+                const ctxs = TokenDetailPane.detailedPane.chart.find("canvas").get(0).getContext('2d')
 
                 const gradient = ctxs.createLinearGradient(0, 0, 0, 150)
                 let lineColor = 'rgba(22,199,132,1)'
@@ -207,21 +234,21 @@ class TokenDetailPane {
                             const x = chart.scales.x.getValueForPixel(canvasPosition.x)
                             const data = chart.data.datasets[0].data
                             const key = Object.keys(data)[x]
-                            TokenDetailPane.chartInfos.price.html("$" + Utils.beautifyAmount(data[key]))
+                            TokenDetailPane.detailedPane.chartInfos.price.html("$" + Utils.beautifyAmount(data[key]))
 
                             const val0Key = Object.keys(data)[0]
                             const initialPrice = data[val0Key]
                             const variation = (data[key]/initialPrice-1)*100
-                            TokenDetailPane.chartInfos.changeVal.html(Math.abs(variation).toFixed(2))
+                            TokenDetailPane.detailedPane.chartInfos.changeVal.html(Math.abs(variation).toFixed(2))
                             if(variation >= 0)
-                                TokenDetailPane.chartInfos.change.removeClass("negative")
+                                TokenDetailPane.detailedPane.chartInfos.change.removeClass("negative")
                             else
-                                TokenDetailPane.chartInfos.change.addClass("negative")
+                                TokenDetailPane.detailedPane.chartInfos.change.addClass("negative")
                         }
                     }
                 })
 
-                TokenDetailPane.chartLoading.hide()
+                TokenDetailPane.detailedPane.chartLoading.hide()
 
             })
         })
@@ -241,16 +268,16 @@ class TokenDetailPane {
     }
 
     initNews(){
-        TokenDetailPane.newsContainer.html()
-        TokenDetailPane.newsLoading.show()
+        TokenDetailPane.detailedPane.newsContainer.html()
+        TokenDetailPane.detailedPane.newsLoading.show()
 
         fetch("http://virgo.net/fakenewsapi/").then(function(resp){
             resp.json().then(function(json){
 
-                TokenDetailPane.newsLoading.hide()
+                TokenDetailPane.detailedPane.newsLoading.hide()
 
                 for(let news of json){
-                    const elem = TokenDetailPane.sampleNews.clone()
+                    const elem = TokenDetailPane.detailedPane.sampleNews.clone()
                     elem.css("background", "url('"+news.image+"')")
                     elem.find(".title").html(news.name)
 
@@ -262,7 +289,7 @@ class TokenDetailPane {
                         window.open(news.link, "_blank")
                     })
 
-                    TokenDetailPane.newsContainer.append(elem)
+                    TokenDetailPane.detailedPane.newsContainer.append(elem)
                     elem.show()
                 }
 
@@ -273,22 +300,41 @@ class TokenDetailPane {
     initInfos(){
         const _this = this
 
-        fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids="+this.tokenInfos.CG_ID+"&order=market_cap_desc&per_page=100&page=1&sparkline=false")
+        TokenDetailPane.detailedPane.infosLoading.show()
+        TokenDetailPane.detailedPane.infosWrapper.hide()
+
+        fetch("https://api.coingecko.com/api/v3/coins/"+this.tokenInfos.CG_ID+"?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false")
             .then(function(resp){
                 resp.json().then(function(cgInfos){
-                    cgInfos = cgInfos[0]
-                    console.log(cgInfos)
-                    TokenDetailPane.infos.find(".cgRank").html("#" + cgInfos.market_cap_rank)
-                    TokenDetailPane.infos.find(".marketcap").html("$" + Utils.beautifyAmount(cgInfos.market_cap))
-                    TokenDetailPane.infos.find(".volume").html("$" + Utils.beautifyAmount(cgInfos.total_volume))
-                    TokenDetailPane.infos.find(".circulating").html(Utils.beautifyAmount(cgInfos.circulating_supply) + " " + _this.data.ticker)
-                    TokenDetailPane.infos.find(".totalSupply").html(Utils.beautifyAmount(cgInfos.total_supply) + " " + _this.data.ticker)
+                    if(cgInfos.coingecko_rank != null)
+                        TokenDetailPane.detailedPane.infos.find(".cgRank").html("#" + cgInfos.coingecko_rank)
+                    else
+                        TokenDetailPane.detailedPane.infos.find(".cgRank").html("#-")
 
-                    if(cgInfos.max_supply != null)
-                        TokenDetailPane.infos.find(".maxSupply").html(Utils.beautifyAmount(cgInfos.max_supply) + " " + _this.data.ticker)
+                    TokenDetailPane.detailedPane.infos.find(".marketcap").html("$" + Utils.beautifyAmount(cgInfos.market_data.market_cap.usd))
+                    TokenDetailPane.detailedPane.infos.find(".volume").html("$" + Utils.beautifyAmount(cgInfos.market_data.total_volume.usd))
+                    TokenDetailPane.detailedPane.infos.find(".circulating").html(Utils.beautifyAmount(cgInfos.market_data.circulating_supply) + " " + _this.data.ticker)
+                    TokenDetailPane.detailedPane.infos.find(".totalSupply").html(Utils.beautifyAmount(cgInfos.market_data.total_supply) + " " + _this.data.ticker)
 
-                    TokenDetailPane.infos.find(".ath").html("$" + Utils.beautifyAmount(cgInfos.ath))
-                    TokenDetailPane.infos.find(".atl").html("$" + Utils.beautifyAmount(cgInfos.atl))
+                    if(cgInfos.market_data.max_supply != null)
+                        TokenDetailPane.detailedPane.infos.find(".maxSupply").html(Utils.beautifyAmount(cgInfos.market_data.max_supply) + " " + _this.data.ticker)
+                    else
+                        TokenDetailPane.detailedPane.infos.find(".maxSupply").html("-")
+
+                    TokenDetailPane.detailedPane.infos.find(".ath").html("$" + Utils.beautifyAmount(cgInfos.market_data.ath.usd))
+                    TokenDetailPane.detailedPane.infos.find(".atl").html("$" + Utils.beautifyAmount(cgInfos.market_data.atl.usd))
+
+                    let descText = jQuery("<p>"+cgInfos.description.en+"</p>").text()
+                    if(descText.length > 320){
+                        descText = descText.substring(0, 170) + "..."
+                        TokenDetailPane.detailedPane.descReadMore.show()
+                    }else{
+                        TokenDetailPane.detailedPane.descReadMore.hide()
+                    }
+                    TokenDetailPane.detailedPane.infos.find(".desc").html(descText)
+
+                    TokenDetailPane.detailedPane.infosLoading.hide()
+                    TokenDetailPane.detailedPane.infosWrapper.show()
                 })
             })
     }
