f89db7df619539af58ff657f7f302cc10144c9de
coco <corentin.pomme@epitech.eu>
2023-01-18 15:12:04 +0100

    custom element for edit fees add to approveToken.html

--- ui/js/approveToken.js
+++ ui/js/approveToken.js
@@ -4,10 +4,11 @@ function get(name){
     return p[name];
 }
 
-
+let tag;
+tag = document.querySelector("edit-fees")
 let finalGasPrice = 0
 $("#allow").click(async () => {
-    await browser.runtime.sendMessage({command: 'resolveWeb3Authorization', id: get("id"), decision: true, params: {gasPrice: finalGasPrice}})
+    await browser.runtime.sendMessage({command: 'resolveWeb3Authorization', id: get("id"), decision: true, params: {gasPrice: tag.getGas()}})
     window.close()
 })
 
@@ -45,98 +46,15 @@ $("svg").attr("data-jdenticon-value",get("addr"))
 $("#medium").addClass("selected")
 $("#medium").find('.check').css("display","flex")
 
-let feesModifier
 function estimateFees() {
-    $("#allow").attr("disabled", true)
-    getBalance(ticker).then(function(balance){
-        getGasPrice().then(function(gasPrice){
-            const slow = $("#slow")
-            const medium = $("#medium")
-            const fast = $("#fast")
-
-            if (document.getElementById('slow').classList.contains("selected")){
-                 feesModifier = 0.8
-            }
-
-            if (document.getElementById('medium').classList.contains("selected")){
-                 feesModifier = 1
-            }
-
-            if (document.getElementById('fast').classList.contains("selected")){
-                 feesModifier = 1.2
-            }
-
-            finalGasPrice = Math.round(gasPrice * feesModifier)
-
-            let nativeTotal = gas * finalGasPrice
-
-
-            $("#fees").html(Utils.formatAmount(gas * finalGasPrice, decimals))
-
-            $("#slow").click(function (){
-                slow.addClass("selected")
-                slow.find('.label').addClass("pl-0")
-                $("#slow").find('.check').css("display","flex")
-
-                if (document.getElementById('medium').classList.contains("selected")){
-                    medium.removeClass("selected")
-                    medium.find('.label').removeClass("pl-0")
-                    $("#medium").find('.check').css("display","none")
-                }
-
-                if (document.getElementById('fast').classList.contains("selected")){
-                    fast.removeClass("selected")
-                    fast.find('.label').removeClass("pl-0")
-                    $("#fast").find('.check').css("display","none")
-                }
-            })
-
-            $("#medium").click(function (){
-                medium.addClass("selected")
-                medium.find('.label').addClass("pl-0")
-                $("#medium").find('.check').css("display","flex")
-                if (document.getElementById('slow').classList.contains("selected")){
-                    slow.removeClass("selected")
-                    slow.find('.label').removeClass("pl-0")
-                    $("#slow").find('.check').css("display","none")
-                }
-                if (document.getElementById('fast').classList.contains("selected")){
-                    fast.removeClass("selected")
-                    fast.find('.label').removeClass("pl-0")
-                    $("#fast").find('.check').css("display","none")
-                }
-            })
-
-            $("#fast").click(function (){
-                fast.addClass("selected")
-                fast.find('.label').addClass("pl-0")
-                $("#fast").find('.check').css("display","flex")
-                if (document.getElementById('slow').classList.contains("selected")){
-                    slow.removeClass("selected")
-                    slow.find('.label').removeClass("pl-0")
-                    $("#slow").find('.check').css("display","none")
-                }
-                if (document.getElementById('medium').classList.contains("selected")){
-                    medium.removeClass("selected")
-                    medium.find('.label').removeClass("pl-0")
-                    $("#medium").find('.check').css("display","none")
-                }
-            })
+    getBalance(ticker).then(function(balance) {
+        getGasPrice().then(function (res) {
+            tag.start(gas, res)
+            console.log()
+            let nativeTotal = gas * tag.getGas()
 
             if(nativeTotal <= balance.balance)
                 $("#allow").attr("disabled", false)
-
-            $(".editFees").click(function (){
-                $("#editFees").css("display","block")
-                finalGasPriceSlow = Math.round(gasPrice * 0.8)
-                finalGasPriceMedium = Math.round(gasPrice * 1)
-                finalGasPriceFast = Math.round(gasPrice * 1.2)
-
-                $("#editFeesSlow").html(Utils.formatAmount(gas * finalGasPriceSlow, decimals))
-                $("#editFeesMedium").html(Utils.formatAmount(gas * finalGasPriceMedium, decimals))
-                $("#editFeesFast").html(Utils.formatAmount(gas * finalGasPriceFast, decimals))
-
-            })
         })
     })
 }
@@ -155,7 +73,6 @@ $("#closeFees").click(function (){
 
 
 setInterval(function(){
-    console.log(Utils.formatAmount(gas*finalGasPrice,decimals))
     estimateFees()
 }, 2500)
 
