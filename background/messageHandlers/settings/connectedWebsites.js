3c8bc5c9b7698795d26ffe5cdfa24ccc6c86e485
PRAXlS <lalin.hugo@gmail.com>
2025-09-19 20:43:31 +0200

    Fix disconnected websites not being persisted

--- background/messageHandlers/settings/connectedWebsites.js
+++ background/messageHandlers/settings/connectedWebsites.js
@@ -9,10 +9,12 @@ class ConnectedWebsitesHandlers {
         {
             if (connectedWebsites[i] === request.address) {
                 connectedWebsites.splice(i, 1)
+                browser.storage.local.set({"connectedWebsites": connectedWebsites})
                 sendResponse({'accepted': true,'siteLength' : connectedWebsites.length})
                 break
             }else if(connectedWebsites[i].type === "walletConnect" && connectedWebsites[i].params.topic === request.address){
                 connectedWebsites.splice(i, 1)
+                browser.storage.local.set({"connectedWebsites": connectedWebsites})
                 walletConnect.disconnect(request.address)
                 sendResponse({'accepted': true,'siteLength' : connectedWebsites.length})
                 break
